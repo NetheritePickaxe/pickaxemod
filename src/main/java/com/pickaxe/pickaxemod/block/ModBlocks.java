4269f25c50cb75ece56bfd9202a2207af82ca6ca
NetheritePickaxe <gsc8379@163.com>
2026-06-02 21:43:57 +0800

    feat: 完善方块逻辑

--- src/main/java/com/pickaxe/pickaxemod/block/ModBlocks.java
+++ src/main/java/com/pickaxe/pickaxemod/block/ModBlocks.java
@@ -5,7 +5,9 @@ import com.pickaxe.pickaxemod.item.ModItems;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -17,7 +19,14 @@ public class ModBlocks {
             DeferredRegister.createBlocks(PickaxeMod.MODID);
 
     public static final DeferredBlock<Block> PICKAXE_BLOCK =
-            registerBlocks("pickaxe_block", () -> new ModDirectionalBlock(BlockBehaviour.Properties.of().strength(1.5F, 6.0F)));
+            registerBlocks("pickaxe_block", () -> new ModDirectionalBlock(BlockBehaviour.Properties.of()
+                    .mapColor(MapColor.COLOR_BLACK)
+                    .requiresCorrectToolForDrops()
+                    .strength(50.0F, 1200.0F)
+                    .sound(SoundType.NETHERITE_BLOCK)
+                    .lightLevel((state) -> 8)
+            ));
+
 
     private static <T extends Block> void registerBlockItems(String name, DeferredBlock<T> block) {
         ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
